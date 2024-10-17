5d803754341291cd52fb4fc5ee290e7b13e23c3a
Rolando MoralesQuintero <128012102+Knavish817@users.noreply.github.com>
2024-10-17 14:34:34 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("bienvenido a screen match ");
+        System.out.println("Pelicula:matrix");
+
+        int fechaDeLanzamiento=1999;
+        boolean incluidoEnElPlan=true;
+        double notaDeLaPelicula=8.2;
+        double media= (8.2+6.9)
+        System.out.println();
+    }
+}
