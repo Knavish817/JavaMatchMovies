e86839e3cf8ac65aeda5c23c8258b61a35218896
Rolando MoralesQuintero <128012102+Knavish817@users.noreply.github.com>
2024-10-22 22:04:29 -0600

    Update2 Main.java

--- Main.java
+++ Main.java
@@ -8,6 +8,21 @@ public class Main {
         boolean incluidoEnElPlan=true;
         double notaDeLaPelicula=8.2;
         double media= (8.2+6.9)
-        System.out.println();
+        System.out.println(media);
+
+        String sinopsis = """
+                Matrix es una paradoja 
+                La mejor pelicula del fin del milenio 
+                Fue lanzada en: """ + fechaDeLanzamiento; //uso de text block para producir la sinosis
+
+        String menu= """
+                MENU:
+                1.Matrix 
+                2.Spiderman
+                3.Batman 
+                4.Sherk 
+                """
+        System.out.println(sinopsis);
+
     }
 }
